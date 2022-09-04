3d47fae68b91f86ecfbe6683a07c016b911729f4
Ivan Makarov <ivan.makarov199716@gmail.com>
2022-09-04 21:56:11 +0300

    Исправил ошибку в поисковом запросе

--- test_google_search.py
+++ test_google_search.py
@@ -3,7 +3,7 @@ from selene import be, have, driver,by
 
 #выполняем поиск
 def test_search_selene():
-    browser.element('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').should(be.blank).type('!234!').press_enter()
+    browser.element('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').should(be.blank).type('selene').press_enter()
     browser.element('//*[@id="rso"]/div[1]/div/div/div[1]/div/a/h3').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
 
 
