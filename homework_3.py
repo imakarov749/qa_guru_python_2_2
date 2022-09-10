f35ecc0ea363649ddb9e285dc331e330ed968712
Ivan Makarov <ivan.makarov199716@gmail.com>
2022-09-10 14:18:45 +0300

    Сделал функцию, которая печатает читаемое имя переданной ей функции и значений аргументов

---
+++ homework_3.py
@@ -0,0 +1,20 @@
+def func_print(func_name, *args):
+    func_name = func_name.__name__
+    print(func_name, *args)
+
+
+def open_browser(browser_name):
+    func_print(open_browser, browser_name)
+
+
+def go_to_company_name_homepage(page_url):
+    func_print(go_to_company_name_homepage, page_url)
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    func_print(find_registration_button_on_login_page, page_url, button_text)
+
+
+open_browser('Google Chrome')
+go_to_company_name_homepage('https://vk.com/')
+find_registration_button_on_login_page('https://vk.com/', 'Зарегистрироваться')
