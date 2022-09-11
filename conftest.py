6fe36b7a4e0577c6610ebdd254dd4c754b863e5f
Ivan Makarov <ivan.makarov199716@gmail.com>
2022-09-11 18:23:29 +0300

    Добавил фикстуру для формы demoqa

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 from selene.support.shared import browser
 import pytest
-from selene import have
+from selene import have, be
 
 #открываем браузер, проверяем корректность url закрываем после теста
 @pytest.fixture()
@@ -14,8 +14,20 @@ def open_browser():
     yield
     browser.close()
 
+
+#открываем браузер, проверяем корректность url закрываем после теста
+@pytest.fixture()
+def demo_qa_open_browser():
+    browser.open('https://demoqa.com/automation-practice-form')
+
+    # проверяем, что открытый url корректный
+    browser.should(have.url('https://demoqa.com/automation-practice-form'))
+    browser.element('.main-header').should(be.visible)
+    yield
+    browser.close()
+
 #задаем конфигурацию браузера
 @pytest.fixture(autouse=True)
-def browser_configure(open_browser):
+def browser_configure(demo_qa_open_browser):
     browser.config.hold_browser_open = True
     browser.driver.set_window_size(1920, 1080)
